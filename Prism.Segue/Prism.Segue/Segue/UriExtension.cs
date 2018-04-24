cb7feab7fcbc1fdf93805715193adc01cdb658bc
Chase Florell <chase@flomedia.ca>
2018-04-23 20:37:41 -0700

    [CF] explicite and formatting

--- Prism.Segue/Prism.Segue/Segue/UriExtension.cs
+++ Prism.Segue/Prism.Segue/Segue/UriExtension.cs
@@ -13,7 +13,11 @@ namespace Prism.Segue.Application.Segue
 {
     public enum UseModalNavigation
     {
-        Auto,Modal,Hierarchical,GoBack,BackToRoot
+        Auto,
+        Modal,
+        Hierarchical,
+        GoBack,
+        BackToRoot
     }
 
     [ContentProperty(nameof(UseModalNavigation))]
@@ -39,8 +43,7 @@ namespace Prism.Segue.Application.Segue
             _navigating = true;
             RaiseCanExecuteChanged();
             InitNavService();
-
-
+            
             if (_navService != null)
             {
                 bool? useModalNavigation = null;
@@ -58,6 +61,10 @@ namespace Prism.Segue.Application.Segue
                     case UseModalNavigation.GoBack:
                         await _navService.GoBackAsync();
                         return;
+                    case UseModalNavigation.Auto:
+                    default:
+                        useModalNavigation = null;
+                        break;
                 }
 
                 switch (parameter)
@@ -70,16 +77,29 @@ namespace Prism.Segue.Application.Segue
                         break;
                 }
             }
+
             _navigating = false;
             RaiseCanExecuteChanged();
         }
 
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (serviceProvider == null) throw new ArgumentNullException("serviceProvider");
+
+            _rootObjectProvider = serviceProvider.GetService(typeof(IRootObjectProvider)) as IRootObjectProvider;
+            _valueTargetProvider = serviceProvider.GetService(typeof(IProvideValueTarget)) as SimpleValueTargetProvider;
+
+            return this;
+        }
+
         private void InitNavService()
         {
+            if (_navService != null) return;
             // if XamlCompilation is active, IRootObjectProvider is not available, but SimpleValueTargetProvider is available
             // if XamlCompilation is inactive, IRootObjectProvider is available, but SimpleValueTargetProvider is not available
             object rootObject;
-            if (_rootObjectProvider == null && _valueTargetProvider == null) throw new ArgumentException("serviceProvider does not provide an IRootObjectProvider or SimpleValueTargetProvider");
+            if (_rootObjectProvider == null && _valueTargetProvider == null)
+                throw new ArgumentException("serviceProvider does not provide an IRootObjectProvider or SimpleValueTargetProvider");
             if (_rootObjectProvider == null)
             {
                 PropertyInfo propertyInfo = _valueTargetProvider.GetType().GetTypeInfo().DeclaredProperties.FirstOrDefault(dp => dp.Name.Contains("ParentObjects"));
@@ -88,32 +108,16 @@ namespace Prism.Segue.Application.Segue
                 var parentObjects = propertyInfo.GetValue(_valueTargetProvider) as IEnumerable<object>;
                 var parentObject = parentObjects.FirstOrDefault(pO => pO.GetType().GetTypeInfo().IsSubclassOf(typeof(Page)));
 
-                rootObject =  parentObject ?? throw new ArgumentNullException("parentObject");
+                rootObject = parentObject ?? throw new ArgumentNullException("parentObject");
             }
             else
             {
-                rootObject = _rootObjectProvider.RootObject ;
+                rootObject = _rootObjectProvider.RootObject;
             }
 
-            if (_navService == null && rootObject is Page page && page.BindingContext is ViewModelBase vm)
-            {
-                _navService = vm.NavigationService;
-            }
+            if (rootObject is Page page && page.BindingContext is ViewModelBase vm) _navService = vm.NavigationService;
         }
 
-        public object ProvideValue(IServiceProvider serviceProvider)
-        {
-            if (serviceProvider == null) throw new ArgumentNullException("serviceProvider");
-
-             _rootObjectProvider = serviceProvider.GetService(typeof(IRootObjectProvider)) as IRootObjectProvider;
-             _valueTargetProvider = serviceProvider.GetService(typeof(IProvideValueTarget)) as SimpleValueTargetProvider;
-
-            return this;
-        }
-
-        private void RaiseCanExecuteChanged()
-        {
-            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
-        }
+        private void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
     }
 }
